39eaae9ad7d20edcb48daf232a0ad9f5b01bf9cf
Muhammed Iqbal P B <iqbalbasheer646@gmail.com>
2021-11-17 17:56:02 +0530

    F string added

--- bmi.py
+++ bmi.py
@@ -2,4 +2,6 @@ height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
 
 bmi = (weight/((height)**2))
-print(int(bmi))
+bmi_int = int(bmi)
+
+print(f"Your BMI is {bmi_int}")
